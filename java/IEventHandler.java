d10cceea996bb644265f6f2d101170f19e30a4ea
Wurzelstumpf <katzmatt@iinet.net.au>
2016-02-02 22:17:44 +1100

    Add Copyright and GNU License

--- java/IEventHandler.java
+++ java/IEventHandler.java
@@ -1,7 +1,22 @@
 //**********************************************************************
-//
 // cresendo - EventEngine
 //
+//  Copyright (C) 2008,2009 Mark Matthews
+//  Distributed under the terms of the GNU General Public License
+//
+//  This file is part of Cresendo.
+//  Cresendo is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  Cresendo is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with Cresendo.  If not, see <http://www.gnu.org/licenses/>.
 //***********************************************************************
 
 import com.tivoli.tec.event_delivery.TECEvent;
